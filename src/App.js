ad639a9bfa0d7d20a4e6e8fc936a560e75df7573
Willian <willianx64@gmail.com>
2022-07-08 09:17:24 -0300

    fix: auto open quotation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Header from "./components/Header";
-import { Quotation } from "./components/Quotation";
+import Routes from "./routes";
 import GlobalStyle from "./styles/global";
 
 function App() {
   return (
     <div className="App">
-        <GlobalStyle />
-        <Header />
-        <Quotation/>
+      <GlobalStyle />
+      <Routes />
     </div>
   );
 }
