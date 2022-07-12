fff36433fc81f7ec2e356d7f23eb33262c88bed9
Willian <willianx64@gmail.com>
2022-07-12 16:41:06 -0300

    fix: reduce gap

--- src/components/DailyNews/styles.js
+++ src/components/DailyNews/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledDailyNewsList = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  gap: 20px;
+  gap: 10px;
   list-style: none;
   justify-content: space-between;
 `;
--- src/pages/news/index.jsx
+++ src/pages/news/index.jsx
@@ -1,3 +1,4 @@
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import NewsArea from "../../components/NewsArea";
 import { NewsAreaMain, NewsContent } from "./styles";
@@ -11,6 +12,7 @@ const News = () => {
           <NewsArea />
         </NewsContent>
       </NewsAreaMain>
+      <Footer />
     </>
   );
 };
